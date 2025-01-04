83951f05c8bfab0ba32c65fc7c3d81086379589c
wglee0511 <ranazang@naver.com>
2025-01-04 20:07:22 +0900

    feature: 32

---
+++ src/javascript/32.ts
@@ -0,0 +1,7 @@
+const strObj = new String("jimin")
+
+console.log('strObj: ', strObj); // [String: 'jimin']
+ // [String: '']
+
+const hello = "hello world"
+console.log(hello.substring(2, 7)); // llo w
